06fe5af5db14b0744c815a710ab6537ae3851cbb
Chiputao <420958094@qq.com>
2024-02-19 20:34:46 -0800

    handle parents

--- src/grouping.tsx
+++ src/grouping.tsx
@@ -1,34 +1,80 @@
-// methods for grouping items by color, shape, item type
-// import function with keywords export into app.tsx
-
 import { BoardNode } from "@mirohq/websdk-types";
 
-// color grouping - should lookup for types with attribute color, e.g. sticker, shape,
-export async function groupItems() {
-    const items = await miro.board.get();
-    // TODO: check color output
+interface HierarchyElement {
+    id: string;
+    type: string; //"frame", "group", "shape"
+    parentId?: string // if it has ancestor
+    children?: BoardNode[];
+}
+
+// build hierarchy
+async function collectItemsByFrame(items : BoardNode[]) {
+    // {frame1:[node1, node2,..], frame2: [node1, node2,...]}
+    const frameMap : { [key: string]: BoardNode[]} = {}
+
+    for (const item of items) {
+        if (item.type === "frame" && item.childrenIds.length > 0) {
+                const children = await miro.board.get( {id: item.childrenIds});
+                frameMap[item.id] = children;
+            }
+            // todo: handle nested cases
+        }
+        return frameMap;
+    }
+
+function collectItemsByGroup() {
+    // impl
+}
+
+function collectItemsByVisual() {
+    // impl
+}
+
+function collectItemByDistance() {
+    // impl
+}
+
+// helper
+function determineColor(item : BoardNode) {
+    let color;
+    if (item.type === "card") {
+        color = item.style.cardTheme; // hex
+    } else if (item.type === "shape") {
+        // cases: same border color & fill color, only have fill color/border color, different boarder color and fill color
+        color = item.style.fillColor !== "transparent" ? item.style.fillColor : item.style.borderColor; // hex
+    } else if (item.type === "sticky_note") {
+        color = item.style.fillColor; // not hex
+    } else if (item.type === "text") {
+        color = item.style.color; // hex
+    }
+    return color;
+}
+
+// call this function after collecting items by different hierarchy function
+async function groupItems(children : BoardNode[]) {
+
     const itemMap: { [key: string]: BoardNode[] } = {};
 
-    items.forEach((item) => {
-        let color;
-        if (item.type === "card") {
-            color = item.style.cardTheme;
-        } else if (item.type === "shape") {
-            // have same border color & fill color, only have fill color, have different boarder color and fill color,
-            // TBD: group by fill color for now
-            color = item.style.fillColor !== "transparent" ? item.style.fillColor : item.style.borderColor;
-        } else if (item.type === "sticky_note") {
-            color = item.style.fillColor;
-        } else if (item.type === "text") {
-            color = item.style.color;
-        }
-
+    // {color_string: [item1, item1,..]}
+    for (const child of children) {
+        const color = determineColor(child);
         if (color in itemMap) {
-            itemMap[color].push(item); // TODO: debug, got "undefined 1undefined: undefined"
+            itemMap[color].push(child);
         } else {
-            itemMap[color] = [item];
+            itemMap[color] = [child];
         }
-    });
-
+    }
     return itemMap;
+};
+
+// todo
+export async function getHierarchyGroups() {
+    const items = await miro.board.get();
+    const hierarchyMap : { [key: string]: { [key: string]: BoardNode[] } } = {}; // {frameid: {color: [node1, node2, ..]}}
+    const frameMap = collectItemsByFrame(items);
+    for (const [frameId, children] of Object.entries(frameMap)) {
+        hierarchyMap[frameId] = await groupItems(children); // Await the groupItems function call
+    }
+
+    return hierarchyMap;
 }
