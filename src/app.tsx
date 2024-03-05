a0d6a24c3f780fb792bc1e1b02ead1440a782687
koolgax99 <nihar.sanda@gmail.com>
2024-03-05 17:47:16 -0500

    Removed commented code for app of notifs

--- src/app.tsx
+++ src/app.tsx
@@ -1,6 +1,5 @@
 import * as React from "react";
 import { createRoot } from "react-dom/client";
-import { BoardNode } from "@mirohq/websdk-types";
 
 import "../src/assets/style.css";
 import { groupItems } from "./grouping";
@@ -26,35 +25,6 @@ const App: React.FC = () => {
     }
 
     fetchData();
-
-    // WIP: notification feature
-    // TODO(hy): gernalize the event listener, it is doable to have one function and pass the event type as a parameter
-
-  //   // Listen to the 'items:create' event.
-  //   miro.board.ui.on("items:create", async (event) => {
-  //     // array of created items
-  //     const createdItems = event.items;
-  //     // update messages
-  //     const newMessages = createdItems.map(
-  //       (item: { type: string; createdBy: string }) => {
-  //         return `A new ${item.type} was created by userId: ${item.createdBy}`;
-  //       }
-  //     );
-  //     setMessages((prevMessages) => [...prevMessages, ...newMessages]);
-  //   });
-
-  //   // Listen to the 'items:delete' event.
-  //   miro.board.ui.on("items:delete", async (event) => {
-  //     // array of deleted items
-  //     const deletedItems = event.items;
-  //     // update messages
-  //     const newMessages = deletedItems.map(
-  //       (item: { type: string; createdBy: string }) => {
-  //         return `A ${item.type} was deleted by userId: ${item.createdBy}`;
-  //       }
-  //     );
-  //     setMessages((prevMessages) => [...prevMessages, ...newMessages]);
-  //   });
   }, []);
 
   return (
