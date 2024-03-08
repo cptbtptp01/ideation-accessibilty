eeea2c0ef5528348648de133b52a5866663ad38a
Chiputao <420958094@qq.com>
2024-03-08 13:18:56 +0900

    update groupingList to reflect current grouping output

--- src/components/groupingList.tsx
+++ src/components/groupingList.tsx
@@ -1,30 +1,88 @@
-import { BoardNode } from "@mirohq/websdk-types";
 import * as React from "react";
 
+interface GroupContent {
+  title: string;
+  content: any; // string[] or obj
+}
+
+export interface Cluster {
+  title: string;
+  content: { [key: string]: GroupContent } | string[];
+}
 interface Props {
-  groups: string;
+  groups: { [key: string]: Cluster };
   onUpdateGrouping: () => void;
 }
 
 const GroupingList: React.FC<Props> = ({ groups, onUpdateGrouping }) => {
+  const initialAnnouncement =
+    `There are ${Object.keys(groups).length} parts of the board. ` +
+    Object.keys(groups)
+      .map((clusterKey) => {
+        return `${clusterKey} is about ${groups[clusterKey].title}.`;
+      })
+      .join(" ");
+
   return (
-    <div
-      className="cs1 ce12"
-      role="region"
-      aria-roledescription="overview"
-      id="summary"
-      aria-labelledby="overviewheading"
-    >
-      <h1 id="overviewheading">Overview</h1>
+    <div className="cs1 ce12" role="region" aria-label="Overview">
+      <h1 aria-label="Overview">Overview</h1>
       <button
-        className="button button-primary button-medium"
+        className="update-overview-button button button-primary button-medium"
         type="button"
         onClick={onUpdateGrouping}
+        aria-label="Update Overview"
       >
         Update Overview
       </button>
-      {/* raw json for now to test the grouping out put */}
-      <pre>{groups}</pre>
+      <div className="sr-only" aria-live="polite">
+        {initialAnnouncement} To continue learning, use Control + Option + Shift
+        + Down arrow.
+      </div>
+      {Object.keys(groups).map((clusterKey) => (
+        <div
+          key={clusterKey}
+          role="group"
+          aria-label={`This group is about ${groups[clusterKey].title}`}
+        >
+          <h2>{groups[clusterKey].title}</h2>
+          {Array.isArray(groups[clusterKey].content) ? (
+            <div
+              role="group"
+              aria-label={`summary for group ${groups[clusterKey]}`}
+            >
+              {groups[clusterKey].content.map((item, index) => (
+                <p key={index} dangerouslySetInnerHTML={{ __html: item }} />
+              ))}
+            </div>
+          ) : (
+            Object.keys(groups[clusterKey].content).map((groupKey) => (
+              <div
+                key={groupKey}
+                role="group"
+                aria-label={`This group is about ${groups[clusterKey].content[groupKey].title}`}
+              >
+                <h3>{groups[clusterKey].content[groupKey].title}</h3>
+                {Array.isArray(groups[clusterKey].content[groupKey].content) ? (
+                  <div>
+                    {groups[clusterKey].content[groupKey].content.map(
+                      (item, index) => (
+                        <p
+                          key={index}
+                          dangerouslySetInnerHTML={{ __html: item }}
+                        />
+                      )
+                    )}
+                  </div>
+                ) : (
+                  <div>
+                    <p>{groups[clusterKey].content[groupKey].content}</p>
+                  </div>
+                )}
+              </div>
+            ))
+          )}
+        </div>
+      ))}
     </div>
   );
 };
